a662d244877d1249c0c678136936850369bd01dc
Jakub Pietrzyk <pietrzykj9@gmail.com>
2024-01-19 19:42:57 +0100

    deleted database connection functions

--- twitter_service/twitter_handler.py
+++ twitter_service/twitter_handler.py
@@ -16,15 +16,6 @@ class TweeterScraper:
         self.cancel_search = False
         self.setup_logging()
         self.lock = threading.Lock()
-        if not os.path.exists("databases"):
-            os.makedirs("databases")
-        try:
-            self.connection = sqlite3.connect(DATABASE_PATH)
-            self.logger.info(f"Connected to database")
-        except sqlite3.Error as e:
-            error_message = f"Error connectiong to database {type(e).__name__} - {str(e)}"
-            self.logger.error(error_message)
-            raise DatabaseConnectionError(error_message)
 
     def setup_logging(self):
         if not os.path.exists("logs"):
@@ -38,12 +29,7 @@ class TweeterScraper:
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
 
-    async def save_tweets_to_database(self, tweets_to_save):
-        self.logger.info(f"Saving data to database")
-        with self.lock:
-            tweets_to_save.to_sql(DATABASE_TABLE_NAME, self.connection, if_exists='append', index=False)
-
-    async def save_tweets_to_csv_and_database(self, all_tweets, keyword):
+    async def save_tweets_to_csv(self, all_tweets, keyword):
         if not os.path.exists("csvs"):
             os.makedirs("csvs")
         tweets_to_add = []
@@ -61,13 +47,12 @@ class TweeterScraper:
         with self.lock:
             self.logger.info(f"Saving data to {filename} file")
             tweets_to_save.to_csv(filename, index=False)
-        await self.save_tweets_to_database(tweets_to_save)
 
     async def search_user_tweets_and_save_to_csv(self, twitter_account):
         app = Twitter(TWITTER_SESSION)
         self.logger.info(f"Start searching for {twitter_account}")
         all_tweets = app.get_tweets(twitter_account, pages=TWEETS_SEARCH_PAGES, wait_time=TWEETS_SEARCH_WAIT_TIME)
-        await self.save_tweets_to_csv_and_database(all_tweets, twitter_account)
+        await self.save_tweets_to_csv(all_tweets, twitter_account)
         self.logger.info(f"Finished searching for {twitter_account}")
 
     async def search_keyword_tweets_and_save_to_csv(self, keyword, until=None, since=None):
@@ -117,7 +102,7 @@ class TweeterScraper:
                 self.logger.error("Received KeyboardInterrupt. Cancelling the search.")
                 self.cancel_search = True
 
-        await self.save_tweets_to_csv_and_database(all_tweets, keyword)
+        await self.save_tweets_to_csv(all_tweets, keyword)
         self.logger.info(f"Finished searching for {keyword} in {num_chunks} chunks")
 
     async def search_tweets(self, app, query, since, until, keyword):
@@ -133,19 +118,5 @@ class TweeterScraper:
             error_message = f"Error while searching for tweets: {type(e).__name__} - {str(e)}"
             self.logger.error(error_message)
 
-        await self.save_tweets_to_csv_and_database(all_tweets, keyword)
+        await self.save_tweets_to_csv(all_tweets, keyword)
         self.logger.info(f"Finished searching for {keyword}")
-
-    async def get_data_from_database_and_convert_to_csv(self):
-        self.logger.info(f"Start get all data from database")
-        query = f"SELECT * FROM {DATABASE_TABLE_NAME}"
-        df = pd.read_sql_query(query, self.connection)
-        if not df.empty:
-            timestamp = date.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            filename = f"csvs/{TWEETS_CSV_NAME}from_db_{timestamp}.csv"
-            df.to_csv(filename, index=False)
-        self.logger.info(f"Finished getting all data from database")
-
-    def close_connection(self):
-        self.logger.info(f"Database connection closed")
-        self.connection.close()
