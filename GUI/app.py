568223558c7fbb46b2bf4ea5d99511fc3ee7cb98
pklinkowski <pklinkowski@student.agh.edu.pl>
2024-01-20 23:06:25 +0100

    GUI with added charts of volume and sentiment

--- GUI/app.py
+++ GUI/app.py
@@ -8,15 +8,14 @@ st.set_page_config(page_title="PiTE_Project", page_icon="💹", layout="wide")
 
 st.title("PiTE_Project")
 
-dict_crypto = {"Verge": "XVG", "TRON": "TRX", "Binance Coin": "BNB"}
+dict_crypto = {"Ripple": "XRP", "OneCoin": "ONE"} # !!!update with all cryptos afterwards!!!
 
 selected_crypto = st.sidebar.selectbox("Select crypto", list(dict_crypto.keys()))
 
 
-# zmienic potem sciezke do pliku
 def get_crypto_data(crypto_name):
     crypto_iso = dict_crypto[crypto_name]
-    crypto_data = pd.read_csv("../Download_and_show_data/cryptocmd_scraper/final_data/" + crypto_iso.lower() + "_data_final.csv", parse_dates=['Date'])
+    crypto_data = pd.read_csv("../csvs/" + crypto_iso.lower() + "_merged.csv", parse_dates=['Date'])
     return crypto_iso, crypto_data
     
 crypto_iso = get_crypto_data(selected_crypto)[0]
@@ -46,8 +45,8 @@ price_fig.add_trace(go.Scatter(x=crypto_data['Date'], y=crypto_data['Close'], mo
 
 # update axes names
 price_fig.update_xaxes(title_text='Date')
-price_fig.update_yaxes(title_text='Price [$]')
-price_fig.update_layout(title_text= selected_crypto + ' price over time')
+price_fig.update_yaxes(title_text='Price [$]', range=[0, 1.3 * crypto_data['Close'].max()])
+price_fig.update_layout(title_text= dict_crypto[selected_crypto] + ' price over time')
 
 # isplay price plot
 col1.plotly_chart(price_fig)
@@ -57,13 +56,22 @@ volume_fig = go.Figure()
 volume_fig.add_trace(go.Scatter(x=crypto_data['Date'], y=crypto_data['Volume'], mode='lines'))
 
 volume_fig.update_xaxes(title_text='Date')
-volume_fig.update_yaxes(title_text='Volume [$]')
-volume_fig.update_layout(title_text= selected_crypto + ' volume over time')
+volume_fig.update_yaxes(title_text='Volume [$]', range=[0, 1.3 * crypto_data['Volume'].max()])
+volume_fig.update_layout(title_text= dict_crypto[selected_crypto] + ' volume over time')
 
 # display volume plot
 col2.plotly_chart(volume_fig)
 
 
+# sentiment plot
+sentiment_fig = go.Figure()
+sentiment_fig.add_trace(go.Scatter(x=crypto_data['Date'], y=crypto_data['compound'], mode='lines'))
+
+sentiment_fig.update_xaxes(title_text='Date')
+sentiment_fig.update_yaxes(title_text='Sentiment', range=[-1,1])
+sentiment_fig.update_layout(title_text= dict_crypto[selected_crypto] + ' opinion on Twitter')
+
+col1.plotly_chart(sentiment_fig)
 
 
 
